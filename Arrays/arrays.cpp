ff199adf7948ffe4c6dfde36425abea66637ce34
gourav-sh <sharmagourav00100@gmail.com>
2026-08-24 23:17:22 +0530

    Add comments to largest element solution

--- Arrays/arrays.cpp
+++ Arrays/arrays.cpp
@@ -1,15 +1,18 @@
-pair<int, int> LargestElement(vector<int> &arr, int n)
-{
-    int largest = arr[0];
-    int index = 0;
-
-    for(int i = 1;i < n; i++)
-    {
-        if(arr[i] > largest)
-        {
-            largest = arr[i];
-            index = i;
-        }
-    }
-    return {largest, index};
+// Find the largest element in the array and return its index
+pair<int, int> LargestElement(vector<int> &arr, int n) 
+{ 
+    int largest = arr[0]; 
+    int index = 0; 
+ 
+    for(int i = 1; i < n; i++) 
+    { 
+        if(arr[i] > largest) 
+        { 
+            largest = arr[i]; 
+            index = i; 
+        } 
+    } 
+    
+    // Return the largest element along with its index
+    return {largest, index}; 
 }
